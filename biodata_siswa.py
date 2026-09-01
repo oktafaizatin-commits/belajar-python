bb238ae35a49f072f537a530b29247d622b440c6
faizaa <oktafaizatin@gmail.com>
2026-09-01 16:41:26 +0700

    Add files via upload

---
+++ biodata_siswa.py
@@ -0,0 +1,22 @@
+#program biodata siswa
+print("=" *35)
+print(" form data siswa")
+print("=" * 35)
+
+nama=input("nama lengkap:")
+kelas=input("kelas      :")
+umur=int(input("umur"(tahun):" ))      
+  tinggi=float(input("tinggi (cm):"))
+                   
+print()
+print("=" * 35)
+print( "data tersimpan")
+print("=" * 35)
+print("nama:",nama)
+print("kelas:"kelas)
+print("umur: "tahun")
+print("tinggi:",tinggi,"cm")
+print("sudah dewasa:",umur>=17)
+
+
+                              
---
+++ eksplorasi_tipe.py
@@ -0,0 +1,17 @@
+#eksplorasi tipe data
+a=100
+b=3.14
+c="smk tjp"
+d=True
+e=10/3
+f=10//3
+
+print("a=",a,"|tipe:",type(a))
+print("b=",b,"|tipe:",type(b))
+print("c=",c,"|tipe:",type(c))
+print("d=",d,"|tipe:",type(d))
+print("e=",e,"|tipe:",type(e))
+print("f=",f,"|tipe:",type(f))
+
+      
+      
